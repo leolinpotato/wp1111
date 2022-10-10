22de6cd8ce7fe07d9b457f869bd33395e3f4b37d
leolin <leolin0904@gmail.com>
2022-10-10 11:58:02 +0800

    first commit

--- hw4/README.md
+++ hw4/README.md
@@ -1,1 +1,19 @@
 # Web Programming HW#4
+## Basic Requirements
+### Initial Layout
+There is a "startGame" state in MineSweeper.js which decides to display HomePage or Board. If "startGame" == false, HomePage will be displayed. Otherwise, Board will be displayed.
+### Start Button
+Once the start button be clicked, the "startGame" will be set to true.
+### FreshBoard
+Create a newBoard using "createBoard" function, and set the board, mineLocations, nonMineCount, remainFlagNum accordingly.
+### Construct the Board using "map"
+Using nested map calling \<Cell> function to construct a 2-D map.
+### Update Flag
+Once the cell is clicked by right click, the "updateFlag" function in Board.js will be called.
+1. If the cell has been revealed, nothing will happen and the function will return. 
+2. If the cell hasn't be revealed and it hasn't be flagged either, its flagged state will be set to true and the remainFlagNum will decrease by one.
+3. If the cell hasn't be revealed but it has be flagged, its flagged state will be set to false and the remainFlagNum will increase by one.
+### Reveal Cell
+Once the cell is clicked by left click, the "revealCell" function in Board.js will be called.
+1. If the cell.value === '💣', "gameOver" state will be set to true.
+2. Otherwise, it will call "revealed" function to update the board. If "nonMineCount" = 0, it will set "gameOver" and "win" to true.
---
+++ hw4/src/components/Board.js
@@ -0,0 +1,121 @@
+/****************************************************************************
+  FileName      [ Board.js ]
+  PackageName   [ src/components ]
+  Author        [ Cheng-Hua Lu ]
+  Synopsis      [ This file generates the Board. ]
+  Copyright     [ 2022 10 ]
+****************************************************************************/
+
+import './css/Board.css'
+import Cell from './Cell';
+import Modal from './Modal';
+import Dashboard from './Dashboard';
+import { revealed } from '../util/reveal';
+import createBoard from '../util/createBoard';
+import React, { useEffect, useState } from 'react';
+
+
+const Board = ({ boardSize, mineNum, backToHome }) => {
+    const [board, setBoard] = useState([]);                     // An 2-dimentional array. It is used to store the board.
+    const [nonMineCount, setNonMineCount] = useState(0);        // An integer variable to store the number of cells whose value are not '💣'.
+    const [mineLocations, setMineLocations] = useState([]);     // An array to store all the coordinate of '💣'.
+    const [gameOver, setGameOver] = useState(false);            // A boolean variable. If true, means you lose the game (Game over).
+    const [remainFlagNum, setRemainFlagNum] = useState(0);      // An integer variable to store the number of remain flags.
+    const [win, setWin] = useState(false);                      // A boolean variable. If true, means that you win the game.
+
+    useEffect(() => {
+        // Calling the function
+        freshBoard();
+    }, []);
+
+    // Creating a board
+    const freshBoard = () => {
+        const newBoard = createBoard(boardSize, mineNum);
+        // Basic TODO: Use `newBoard` created above to set the `Board`.
+        // Hint: Read the definition of those Hook useState functions and make good use of them.
+        setBoard(newBoard.board);
+        setMineLocations(newBoard.mineLocations);
+        setNonMineCount(boardSize*boardSize-mineNum);
+        setRemainFlagNum(mineNum);
+    }
+
+    const restartGame = () => {
+        freshBoard();
+        setGameOver(false);
+        setWin(false);
+    }
+
+    // On Right Click / Flag Cell
+    const updateFlag = (e, x, y) => {
+        // To not have a dropdown on right click
+        e.preventDefault();
+        // Deep copy of a state
+        let newBoard = JSON.parse(JSON.stringify(board));
+        let newFlagNum = remainFlagNum;
+
+        // Basic TODO: Right Click to add a flag on board[x][y]
+        // Remember to check if board[x][y] is able to add a flag (remainFlagNum, board[x][y].revealed)
+        // Update board and remainFlagNum in the end
+        if (newBoard[x][y].revealed)
+            return;
+        if (newBoard[x][y].flagged) {
+            newBoard[x][y].flagged = false;
+            newFlagNum++;
+        }
+        else {
+            newBoard[x][y].flagged = true;
+            newFlagNum--;
+        }
+        setRemainFlagNum(newFlagNum);
+        setBoard(newBoard);
+    };
+
+    const revealCell = (x, y) => {
+        if (board[x][y].revealed || gameOver || board[x][y].flagged) return;
+        /*let newBoard = JSON.parse(JSON.stringify(board));*/
+
+        // Basic TODO: Complete the conditions of revealCell (Refer to reveal.js)
+        // Hint: If `Hit the mine`, check ...?
+        //       Else if `Reveal the number cell`, check ...?
+        // Reminder: Also remember to handle the condition that after you reveal this cell then you win the game.
+        if (board[x][y].value === '💣')
+            setGameOver(true);
+        else {
+            if (nonMineCount == 1) {
+                setGameOver(true);
+                setWin(true);
+            }
+            let outcome = revealed(board, x, y, nonMineCount);
+            setBoard(outcome.board);
+            setNonMineCount(outcome.newNonMinesCount);
+        }
+    };
+
+    return (
+        <div className='boardPage' >
+            <div className='boardWrapper' >
+                {/* Advanced TODO: Implement Modal based on the state of `gameOver` */}
+                {gameOver ? <Modal restartGame={restartGame} backToHome={backToHome} win={win}/> : ""}
+                {/* Basic TODO: Implement Board 
+                Useful Hint: The board is composed of BOARDSIZE*BOARDSIZE of Cell (2-dimention). So, nested 'map' is needed to implement the board.
+                Reminder: Remember to use the component <Cell> and <Dashboard>. See Cell.js and Dashboard.js for detailed information. */} 
+                <div className="boardContainer">
+                    <Dashboard remainFlagNum={remainFlagNum} gameOver={gameOver}/>
+                    {board.map((row, rowIdx) => {
+                        return (
+                            <div key={rowIdx} id={rowIdx} style={{display: 'flex'}}>
+                                {row.map((element, colIdx) => <Cell key={board.length*rowIdx+colIdx} rowIdx={rowIdx} colIdx={colIdx} detail={element} updateFlag={updateFlag} revealCell={revealCell}/>)}
+                            </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    );
+
+
+
+}
+
+export default Board
