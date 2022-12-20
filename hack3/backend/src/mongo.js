f5797377c7b222bc57fe779ab882b5553bf59f25
leolin <leolin0904@gmail.com>
2022-12-20 11:40:56 +0800

    hack3

---
+++ hack3/backend/src/mongo.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { dataInit } from "./upload.js";
+
+import "dotenv-defaults/config.js";
+
+mongoose.set("strictQuery", true);
+
+async function connect() {
+  // TODO 1 Connect to your MongoDB and call dataInit()
+  mongoose.connect(
+      process.env.MONGO_URL, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+      }
+  ).then(async res => {
+        console.log('Reset Mode: reset the data')
+        dataInit()
+  })
+  // TODO 1 End
+}
+
+export default { connect };
---
+++ hack3/frontend/src/components/Analytics.js
@@ -0,0 +1,27 @@
+import { useQuery } from '@apollo/client';
+
+import { GET_ITEMS_QUERY } from '../graphql/queries';
+
+import Balance from './Balance';
+import Category from './Category';
+
+function Analytics() {
+  // TODO 2.2 Use the useQuery hook to get items from backend
+  const {loading, error, data: itemsData, subscribeToMore} = useQuery(GET_ITEMS_QUERY);
+  const {items} = itemsData;
+  // TODO 2.2 End
+
+
+  return (
+    <div className="grid grid-cols-12 gap-6">
+      <div className="col-span-6">
+        <Balance items={items} />
+      </div>
+      <div className="col-span-6">
+        <Category items={items} />
+      </div>
+    </div>
+  );
+}
+
+export default Analytics;
